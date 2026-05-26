4b7be87fe15188b51931d9c487b4d9fb09fcb158
Manuel Odendahl <wesen@ruinwesen.com>
2026-05-26 18:48:51 -0400

    Fix rollout lint and security checks

--- cmd/wsm/cmds/registry/list/repos.go
+++ cmd/wsm/cmds/registry/list/repos.go
@@ -123,9 +123,9 @@ func printReposHuman(repos []wsm.Repository, tags []string) error {
 	}
 
 	var md strings.Builder
-	md.WriteString(fmt.Sprintf("# Repositories (%d)\n\n", len(repos)))
+	_, _ = fmt.Fprintf(&md, "# Repositories (%d)\n\n", len(repos))
 	if len(tags) > 0 {
-		md.WriteString(fmt.Sprintf("> Filter tags: `%s`\n\n", strings.Join(tags, ", ")))
+		_, _ = fmt.Fprintf(&md, "> Filter tags: `%s`\n\n", strings.Join(tags, ", "))
 	}
 
 	for _, repo := range repos {
@@ -148,7 +148,7 @@ func printReposHuman(repos []wsm.Repository, tags []string) error {
 			remote = remote[:93] + "..."
 		}
 
-		md.WriteString(fmt.Sprintf("## %s\n\n", repo.Name))
+		_, _ = fmt.Fprintf(&md, "## %s\n\n", repo.Name)
 		md.WriteString(fmt.Sprintf("- **Branch:** `%s`\n", branch))
 		md.WriteString(fmt.Sprintf("- **Path:** `%s`\n", repo.Path))
 		md.WriteString(fmt.Sprintf("- **Tags:** %s\n", tagsJoined))
